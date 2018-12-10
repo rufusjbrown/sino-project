71f721bd763f9e19946b93ac346572dc2e63e847
rufusjbrown <rufusbrown12@gmail.com>
2018-12-10 22:32:59 +0000

    image formatting

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <!-- <v-card> -->
-        <v-img src="/suzhoujinjilake.png"/>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" flat nuxt to="/opportunities">Find opportunities</v-btn>
-        </v-card-actions>
-      <!-- </v-card> -->
-    </v-flex>
-  </v-layout>
+    <v-layout align-center justify-center>
+        <v-flex xs12 xs6 md8 md6>
+            <v-card flat>
+                <v-img
+                    src="/suzhoujinjilake.png"
+                    aspect-ratio="1.75"
+                ></v-img>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
