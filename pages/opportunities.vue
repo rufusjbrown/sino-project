2d61dc396bac3954f483163c217446bb23650bb8
rufusjbrown <rufusbrown12@gmail.com>
2018-12-10 23:05:24 +0000

    form inputs

--- pages/opportunities.vue
+++ pages/opportunities.vue
@@ -1,43 +1,59 @@
 <template>
   <form>
     <v-text-field
-      v-validate="'required|max:10'"
-      v-model="name"
-      :counter="10"
-      :error-messages="errors.collect('name')"
-      label="Name"
-      data-vv-name="name"
-      required
+        v-validate="'required|max:10'"
+        v-model="userDetails.name"
+        :counter="10"
+        :error-messages="errors.collect('name')"
+        label="Name"
+        data-vv-name="name"
+        required
     ></v-text-field>
+    {{ name }}
     <v-text-field
-      v-validate="'required|email'"
-      v-model="email"
-      :error-messages="errors.collect('email')"
-      label="E-mail"
-      data-vv-name="email"
-      required
+        v-validate="'required|email'"
+        v-model="userDetails.email"
+        :error-messages="errors.collect('email')"
+        label="E-mail"
+        data-vv-name="email"
+        required
     ></v-text-field>
     <v-select
-      v-validate="'required'"
-      :items="items"
-      v-model="select"
-      :error-messages="errors.collect('select')"
-      label="Select"
-      data-vv-name="select"
-      required
+        :items="subjects"
+        v-model="userDetails.expertise"
+        :menu-props="{ maxHeight: '400' }"
+        label="Select your known subjects"
+        multiple
     ></v-select>
+    {{e6}}
     <v-checkbox
-      v-validate="'required'"
-      v-model="checkbox"
-      :error-messages="errors.collect('checkbox')"
-      value="1"
-      label="Option"
-      data-vv-name="checkbox"
-      type="checkbox"
-      required
+        v-validate="'required'"
+        v-model="checkbox"
+        :error-messages="errors.collect('checkbox')"
+        value="1"
+        label="Option"
+        data-vv-name="checkbox"
+        type="checkbox"
+        required
     ></v-checkbox>
-
     <v-btn @click="submit">submit</v-btn>
     <v-btn @click="clear">clear</v-btn>
+    Form Data to send to server:
+    {{ userDetails }}
   </form>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        subjects: ['English', 'Maths', 'Physics'],
+        userDetails: {
+          name: '',
+          email: '',
+          expertise: []
+        }
+      }
+    }
+  }
+</script>
